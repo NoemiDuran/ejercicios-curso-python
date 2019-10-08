b249379e6aaba95747421cae91a1abcaa3cf4707
NoemiDuran <56162967+NoemiDuran@users.noreply.github.com>
2019-10-07 22:38:47 -0500

    Update 1_listas.py

--- data_structures/1_listas.py
+++ data_structures/1_listas.py
@@ -16,13 +16,14 @@ Objetivos de aprendizaje
 
 # 1. Llena la siguiente lista con al menos 3 personajes de tu película favorita
 
-characters = []
+characters = ['Jackie Chan', 'Jack Black', 'Zack Mooneyham']
 
 # 2. Accede al primer elemento de la lista, e imprímelo en pantalla
 # OJO: El código debajo solo es una guía. Reemplazalo con el tuyo
 
-first_character = characters[7]
+first_character = characters[3]
 print(first_character)
+print(characters[0])
 
 
 # 3. Accede al penúltimo elemento, esta vez con números negativos
@@ -30,14 +31,20 @@ print(first_character)
 
 first_character = characters[-5]
 print(first_character)
+print(characters[-1])
 
 # 4. Modifica un elemento de la lista, e imprime la lista
+characters = ['Jackie Chan', 'Jack Black', 'Drake Bell'] #ño
+print(characters)
 
 # Reemplaza esta línea por la modificación en tu lista
 print(characters)
 
 # 5. Añade un nuevo elemento a la lista, al principio y al final, e imprime la lista
 # HINT: Usa dos distintos métodos
+characters = ['Jackie Chan', 'Jack Black', 'Drake Bell']
+characters.append(0, 'Actor X')
+characters.append(3, 'Actor Y')
 
 # Reemplaza esta línea por tus dos lineas de código
 print(characters)
@@ -47,13 +54,16 @@ characters[2] = 'Batman'
 
 # 6. Explica, con comentarios, qué hace cada una de las siguientes líneas de código
 
-del characters[3]
-popped = characters.pop()
-popped_params = characters.pop(0)
-characters.remove('Batman')
+del characters[3]  #elimina el tercer elemento de la lista
+popped = characters.pop() #elimina el ultimo elemento agregado
+popped_params = characters.pop(0) 
+characters.remove('Batman') #elimina el elemento 'Batman'
 
 # 7. Ordena temporalmente la lista, guarda la lista temporal en una variable
 # e imprime tu lista de personajes y la nueva lista. Estas deberían ser diferentes
+characters = ['Jackie Chan', 'Jack Black', 'Drake Bell']
+characters.sort()
+print(characters)
 
 # 8. Ordena para siempre tu lista, e imprímela
 
@@ -61,6 +71,7 @@ characters.remove('Batman')
 
 # 10. Haz la reversa de la siguiente lista
 numbers  = [17, 20, 5, 10, 7]
+numbers.sort(reverse=True)
 
 # 11. Guarda en una variable la longitud de la lista de números
 
