4baef6994588fd01d69b4e854b6b77098e3f6acd
NoemiDuran <56162967+NoemiDuran@users.noreply.github.com>
2019-10-08 22:46:48 -0500

    Update 4_tuplas.py

--- data_structures/4_tuplas.py
+++ data_structures/4_tuplas.py
@@ -7,8 +7,11 @@ Objetivos de aprendizaje
 
 # 1. Dada la siguiente tupla, accede al segundo elemento de ella e imprímelo
 odds = (1, 3, 5, 7)
+print(odds[2])
 
 # 2. Intenta modificar el tercer elemento de la tupla,
 # y explica qué pasó en un comentario
 
 # 3. Convierte la tupla en lista y añade un 9 al final
+list(odds)
+odds.append('9')
