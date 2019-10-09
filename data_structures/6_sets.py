a5256a4c27fb617cf5bd08bf06a51ebfee981dc6
NoemiDuran <56162967+NoemiDuran@users.noreply.github.com>
2019-10-08 23:04:02 -0500

    Update 6_sets.py

--- data_structures/6_sets.py
+++ data_structures/6_sets.py
@@ -11,12 +11,12 @@ Objetivos de aprendizaje
 even = {2, 4, 6, 8, 10}
 
 # 1. Dado el set, añade el 12 al conjunto
-
+even.add(12)
 # 2. Dado el siguiente set, une ambos conjuntos en una nueva variable
 # e imprímela en pantalla (ocupa ambos métodos vistos en clase)
 
 odds = {1, 3, 5, 7, 11, 13}
-
+nueva_variable = 
 # 3. Usando el conjunto de pares (even), haz la interseccion con el siguiente conjunto
 
 numbers = {5, 4, 3, 2, 1}
