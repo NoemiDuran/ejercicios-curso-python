87668c08858e5c783b96f129c7dd152ef5abb59a
NoemiDuran <56162967+NoemiDuran@users.noreply.github.com>
2019-10-08 23:02:04 -0500

    Update 5_diccionarios.py

--- data_structures/5_diccionarios.py
+++ data_structures/5_diccionarios.py
@@ -17,9 +17,14 @@ user = {
 # 1. Imprime en pantalla el nombre del usuario
 
 # 2. Añade un nuevo par llave-valor al diccionario, por ejemplo: licenciatura
+user['licenciatura'] = 'cienciascompu'
 
 # 3. Modifica los lenguajes favoritos de Mauricio, quitando Java de la lista
 
 # 4. Elimina "username" y "birthday" del diccionario usando dos métodos distintos
+#metodo 1
+del user['username']
+del user['birthday']
 
 # 5. Declara un nuevo diccionario vacío
+diccionario = { }
